c9f2c5f5eb8886e07e98ebd3374db42a77ee20b1
Ben Argo <ben@benargo.com>
2014-01-05 17:30:09 +0000

    Working on the JS

--- media/js/client.js
+++ media/js/client.js
@@ -4,11 +4,17 @@ $(function(){
 		var region_name = $(this).val().replace(/ /g,'_').toLowerCase();
 		$.ajax({
 			url: 'http://www.cems.uwe.ac.uk/~b2-argo/atwd/crimes/'+ time_period + '/'+ region_name + '/json',
-			crossDomain: true
-		}).done(function(data) {
-			alert('hi');
-			console.log(data);
-			window.localStorage.setItem('cache_'+ region_name, JSON.stringify(data));
+			crossDomain: true, // with help from http://enable-cors.org/server_php.html
+			ifModified: true
+		}).done(function(data, textStatus, jqXHR) {
+    		if(jqXHR.status == 200)
+    		{
+				window.localStorage.setItem('cache_'+ region_name, JSON.stringify(data));
+			}
+			
+			$.each(data.response.crimes.region.area, function() {
+				
+			});
 			//$.jqplot('bar')
 		});
 	});

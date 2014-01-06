4929fd9a06e124761b2f639d613c70ff0f6b8920
Ben Argo <ben@benargo.com>
2014-01-06 14:48:39 +0000

    Updating various bits of the chart

--- media/js/client.js
+++ media/js/client.js
@@ -7,62 +7,72 @@ $(function(){
 	$('#get-region select[name="select-region"]').change(function() {
 		var time_period = $('#get-region select[name="time-period"]').val();
 		var region_name = $(this).val().replace(/ /g,'_').toLowerCase();
-		$.ajax({
-			type: 'GET',
-			url: 'http://www.cems.uwe.ac.uk/~b2-argo/atwd/crimes/'+ time_period + '/'+ region_name + '/json',
-			crossDomain: true, // with help from http://enable-cors.org/server_php.html
-			ifModified: true,
 
-		}).done(function(data, textStatus, jqXHR) {
-    		if(jqXHR.status == 200)
-    		{
-    			// Override local storage if the data has changed
-				window.localStorage.setItem('cache_'+ region_name, JSON.stringify(data));
-			}
+		if(window.localStorage.getItem('cache_'+ region_name))
+		{
+			var data = window.localStorage.getItem('cache_'+ region_name);
+		}
 
+		// If the region name is valid
+		if(region_name)
+		{
+			$.ajax({
+				type: 'GET',
+				url: 'http://www.cems.uwe.ac.uk/~b2-argo/atwd/crimes/'+ time_period + '/'+ region_name + '/json',
+				crossDomain: true, // with help from http://enable-cors.org/server_php.html
+				ifModified: true,
+			}).done(function(returned_data, textStatus, jqXHR) {
 
-			// Create two new variables for holding the data that we'll pass to the chart library
-			var barData = {
-				labels: ['Areas'],
-				datasets: []
-			};
-			var pieData = [];
+				// Unset the chart data
+				barData = {};
+				pieData = [];
 
-			// Reset the key
-			$('section.chart.key p').remove();
+	    		if(jqXHR.status == 200)
+	    		{
+	    			// Override local storage if the data has changed
+					window.localStorage.setItem('cache_'+ region_name, JSON.stringify(returned_data));
+					var data = returned_data;
+				}
 
-			$.each(data.response.crimes.region.area, function(key, area) {
-				barData.datasets.push({
-					fillColor: chartColors[key],
-					strokeColor: chartColors[key],
-					data: [area.total]
-				})
-				pieData.push({
-					value: area.total,
-					color: chartColors[key]
+				// Create two new variables for holding the data that we'll pass to the chart library
+				var barData = {
+					labels: ['Areas'],
+					datasets: []
+				};
+				var pieData = [];
+
+				// Reset the key
+				$('section.chart.key p').remove();
+
+				$.each(data.response.crimes.region.area, function(key, area) {
+					barData.datasets.push({
+						fillColor: chartColors[key],
+						strokeColor: chartColors[key],
+						data: [area.total]
+					})
+					pieData.push({
+						value: area.total,
+						color: chartColors[key]
+					});
+
+					$('section.chart.key').append('<p class="key item'+ key +'">'+ area.id +'</p>');
 				});
+				
+				// Produce the bar chart
+				$('canvas#bar').remove();
+				$('section.bar.chart').append('<canvas id="bar" height="300" width="700"></canvas>');
+				var ctx = document.getElementById('bar').getContext('2d');
+				var barChart = new Chart(ctx).Bar(barData, {barValueSpacing: 10});
 
-				$('section.chart.key').append('<p class="key item'+ key +'">'+ area.id +'</p>');
+				// Produce the pie chart
+				$('canvas#pie').remove();
+				$('section.pie.chart').append('<canvas id="pie" height="300" width="700"></canvas>');
+				var ctx = document.getElementById('pie').getContext('2d');
+				var pieChart = new Chart(ctx).Pie(pieData, {});
+
+				// Show the charts
+				$('section.chart').show();
 			});
-			
-			// Produce the bar chart
-			$('canvas#bar').remove();
-			$('section.bar.chart').append('<canvas id="bar" height="300" width="700"></canvas>');
-			var ctx = document.getElementById('bar').getContext('2d');
-			var barChart = new Chart(ctx).Bar(barData, {barValueSpacing: 10});
-
-			// Produce the pie chart
-			$('canvas#pie').remove();
-			$('section.pie.chart').append('<canvas id="pie" height="300" width="700"></canvas>');
-			var ctx = document.getElementById('pie').getContext('2d');
-			var pieChart = new Chart(ctx).Pie(pieData, {});
-
-			// Show the charts
-			$('section.chart').show();
-
-			// Unset the chart data
-			barData = {};
-			pieData = [];
-		});
+		}
 	});
 });

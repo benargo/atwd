4a3ab345161cf64483721ca3a9f9a19de66865fd
Ben Argo <ben@benargo.com>
2014-01-11 16:50:01 +0000

    Applying stash 0

--- test/index.php
+++ test/index.php
@@ -282,6 +282,7 @@
 				<p><input type="text" name="username" placeholder="e.g. b2-argo" /> <input type="submit" value="Go" /></p>
 			</form>
 
+<<<<<<< Updated upstream
 			<section id="response">
 				
 			</section>
@@ -289,6 +290,98 @@
 			
 
 			<hr />
+=======
+			<section id="response" style="display: none;">
+				<h2>Result</h2>
+			</section>
+
+			<script type="text/javascript"><!--
+			$('#test-form').submit(function(event)
+			{
+				event.preventDefault();
+				var username = $('input[name="username"]').val().toLowerCase();
+				var base = 'http://www.cems.uwe.ac.uk/~'+ username +'/atwd/crimes';
+				var response = $('#response');
+				var test_urls = [
+					'/6-2013/xml', 
+					'/6-2013/json',
+					'/6-2013/east_of_england/xml',
+					'/6-2013/east_of_england/json',
+					'/6-2013/east_midlands/xml',
+					'/6-2013/east_midlands/json',
+					'/6-2013/london/xml',
+					'/6-2013/london/json',
+					'/6-2013/north_east/xml',
+					'/6-2013/north_east/json',
+					'/6-2013/north_west/xml',
+					'/6-2013/north_west/json',
+					'/6-2013/south_east/xml',
+					'/6-2013/south_east/json',
+					'/6-2013/south_west/xml',
+					'/6-2013/south_west/json',
+					'/6-2013/west_midlands/xml',
+					'/6-2013/west_midlands/json',
+					'/6-2013/yorkshire_and_humber/xml',
+					'/6-2013/yorkshire_and_humber/json',
+					'/6-2013/wales/xml',
+					'/6-2013/wales/json',
+					'/6-2013/british_transport_police/xml',
+					'/6-2013/british_transport_police/json',
+					'/6-2013/action_fraud/xml',
+					'/6-2013/action_fraud/json',
+				];
+
+				function test_url(test_url)
+				{
+					var response_type = test_url.split('/');
+					response_type = response_type[response_type.length-1];
+
+					$.ajax(
+					{
+						async: false,
+						url: '/~b2-argo/atwd/test/ajax_test.php?url='+ base + test_url,
+						method: 'get',
+						statusCode:
+						{
+							404: function() 
+							{
+								response.append('<p class="error 404">'+ base + test_url +' (HTTP 404 Not Found)</p>');
+							},
+							500: function() 
+							{
+								response.append('<p class="error 500">'+ base + test_url +' (HTTP 500 Internal Server Error)</p>');
+							},
+							501: function() 
+							{
+								response.append('<p class="error 501">'+ base + test_url +' (Error 501 URL pattern not recognized)</p>');
+							}
+						},
+						success: function(data, status, xhr) 
+						{
+							switch(xhr.getResponseHeader("content-type"))
+							{
+
+							}
+							response.append('<p class="success">'+ base + test_url +' ()</p>');
+							
+						}
+					});
+				}
+
+
+				test_urls.forEach(function(url)
+				{
+					test_url(url);
+				});
+
+				response.show();
+
+				// 2.1.1 GET All (XML)
+
+				
+			});
+			--></script>
+>>>>>>> Stashed changes
 
 		</article>
 

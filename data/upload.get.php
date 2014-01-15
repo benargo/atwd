795a7fc00e4e4867bc9918504be43797202bc48f
Ben Argo <ben@benargo.com>
2014-01-15 18:50:37 +0000

    Adding google analytics

--- data/upload.get.php
+++ data/upload.get.php
@@ -25,6 +25,21 @@
 	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<script src="../media/js/data-upload.js"></script>
+	<script type="text/javascript">
+	<!--// Google Analytics
+	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-23790873-4']);
+		_gaq.push(['_setDomainName', 'cems.uwe.ac.uk']);
+		_gaq.push(['_trackPageview']);
+
+	 	(function() {
+	    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  	})();
+	-->
+	</script>
 
 </head>
 <body>

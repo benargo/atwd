2f2b8d174b9f5b66833cb4bb6fd37d7ad12b983f
Ben Argo <ben@benargo.com>
2014-02-24 17:48:50 +0000

    Improving the reset script

--- api/reset.php
+++ api/reset.php
@@ -1,5 +1,11 @@
 <?php
 require_once(__DIR__ .'/autoload.php');
+header('Content-type: application/json');
+
+// An empty array to contain all the files
+$deleted_files = array();
+$failed_files = array();
+$success = true;
 
 // Iterate through the custom data folders
 $iterator = new RecursiveDirectoryIterator(BASEDIR.'data/custom');
@@ -7,6 +13,20 @@ foreach (new RecursiveIteratorIterator($iterator) as $filename => $file)
 {
 	if(substr($file->getFileName(), -4) == '.xml')
 	{
-		echo (unlink($file->getPathName()) ? 'Deleted '. $file->getPathName() ."\n" : 'Unable to delete '. $file->getPathName() ."\n");
+		if(unlink($file->getPathName()))
+		{
+			$deleted_files[] = preg_replace('.*data/custom/', '', $file->getPathName());
+		}
+		else
+		{
+			$failed_files[] = preg_replace('.*data/custom/', '', $file->getPathName());
+		}
 	}
 }
+
+$json = array();
+$json['deleted'] = $deleted_files;
+$json['failed'] = $failed_files;
+$json['success'] = $success;
+
+echo json_encode($json);
